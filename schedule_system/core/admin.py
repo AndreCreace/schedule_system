4b1d40b40301466cbffac462c6173537d8c11929
andrecreace <andrecreace@gmail.com>
2021-12-03 11:06:22 -0300

    This commit create the event model to schedule_system

--- schedule_system/core/admin.py
+++ schedule_system/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+# Let´s import the models
+from core.models import Event
+
+# Let´s define, wich Event fiels will be displayed
+class EventAdmin(admin.ModelAdmin):
+    
+    # Let´s define the fields
+    list_display = ("title", "event_date", "created_at")
+    
+    # Let´s create and define wich Event fields, the user can to search.
+    list_filter = ("title", "event_date")
+
 # Register your models here.
+admin.site.register(Event, EventAdmin)
--- schedule_system/core/models.py
+++ schedule_system/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+# Let´s import the Python Models Admin (It´s necessary to create a foreignKey with our tables)
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+# Let´s create the event model class
+class Event(models.Model):
+    
+    # Field (attributs)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField(verbose_name="Event Date")
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Event Time")
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Here, we´re creating the foreing key with the tabela User. We´re still defining cascanding delete.
+    
+    # If I want that the table´s name is different the class name:
+    class Meta:
+        db_table = "event" # event will be the name of table.
+        
+    # Overload the method str
+    def __str__(self):
+        return self.title
